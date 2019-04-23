b70733f7c20456bed2068397dfa8b1ba67f717cf
GavrilovVS <vladimir.s.gavrilov@gmail.com>
2019-04-23 05:43:56 +0100

    Written working with symbol table.

---
+++ include/scope.h
@@ -0,0 +1,84 @@
+/*
+    File:    scope.h
+    Created: 22 April 2019 at 17:20 Moscow time
+    Author:  Гаврилов Владимир Сергеевич
+    E-mails: vladimir.s.gavrilov@gmail.com
+             gavrilov.vladimir.s@mail.ru
+             gavvs1977@yandex.ru
+*/
+
+#ifndef SCOPE_H
+#define SCOPE_H
+#   include <cstdint>
+#   include <cstddef>
+#   include <set>
+#   include <map>
+#   include <boost/optional.hpp>
+#   include <boost/variant.hpp>
+#   include <forward_list>
+#   include <memory>
+namespace lynx_scope{
+    enum class Name_kind : std::uint16_t{
+        Variable,     Function,     Type,
+        Enum,         Struct,       Enum_elem,
+        Struct_field, Cycle_label
+    };
+
+    struct Name_occurence{
+        std::size_t index_of_definition_ = 0;
+        std::size_t attribute_           = 0;
+        Name_kind   kind_                = Name_kind::Variable;
+    };
+
+    bool operator < (const Name_occurence& first, const Name_occurence& second);
+
+    struct Name_info{
+        std::set<Name_occurence> info_;
+    };
+
+    class Scope{
+    public:
+        Scope()                              = default;
+        Scope(const Scope& src)              = default;
+        virtual ~Scope()                     = default;
+        Scope& operator = (const Scope& rhs) = default;
+
+        boost::optional<Name_info> search(std::size_t name_idx);
+        void insert(std::size_t name_idx, const Name_occurence& name_occ);
+    private:
+        std::map<std::size_t, Name_info> scope_symbols_;
+    };
+
+    struct Name_in_global_scope{
+        Name_info info_;
+    };
+
+    struct Name_in_local_scope{
+        Name_info info_;
+    };
+
+    struct Name_is_not_found{};
+
+    using Search_result = boost::variant<Name_in_global_scope,
+                                         Name_in_local_scope,
+                                         Name_is_not_found>;
+
+    class Symbol_table{
+    public:
+        Symbol_table()                                     = default;
+        Symbol_table(const Symbol_table& src)              = default;
+        virtual ~Symbol_table()                            = default;
+        Symbol_table& operator = (const Symbol_table& rhs) = default;
+
+        void add_scope();
+        void delete_scope();
+
+        void insert(std::size_t name_idx, const Name_occurence& name_occ);
+
+        Search_result search(std::size_t name_idx);
+    private:
+        std::forward_list<std::shared_ptr<Scope>> table_;
+        std::size_t                               num_of_scopes_ = 0;
+    };
+};
+#endif
---
+++ src/scope.cpp
@@ -0,0 +1,87 @@
+/*
+    File:    scope.cpp
+    Created: 22 April 2019 at 20:40 Moscow time
+    Author:  Гаврилов Владимир Сергеевич
+    E-mails: vladimir.s.gavrilov@gmail.com
+             gavrilov.vladimir.s@mail.ru
+             gavvs1977@yandex.ru
+*/
+
+#include <tuple>
+#include "../include/scope.h"
+
+namespace lynx_scope{
+   bool operator < (const Name_occurence& first, const Name_occurence& second)
+    {
+        return std::tie(first.index_of_definition_,  first.attribute_,  first.kind_)     <
+               std::tie(second.index_of_definition_, second.attribute_, second.kind_);
+    }
+
+    boost::optional<Name_info> Scope::search(std::size_t name_idx)
+    {
+        auto it = scope_symbols_.find(name_idx);
+        if(it != scope_symbols_.end()){
+            return it->second;
+        }else{
+            return boost::none;
+        }
+    }
+
+    void Scope::insert(std::size_t name_idx, const Name_occurence& name_occ)
+    {
+        auto it = scope_symbols_.find(name_idx);
+        Name_info ninfo;
+        if(it != scope_symbols_.end()){
+            ninfo = it->second;
+        }
+        ninfo.info_.insert(name_occ);
+        scope_symbols_[name_idx] = ninfo;
+    }
+
+    void Symbol_table::add_scope()
+    {
+        table_.push_front(std::make_shared<Scope>());
+        num_of_scopes_++;
+    }
+
+    void Symbol_table::delete_scope()
+    {
+        if(!num_of_scopes_){
+            return;
+        }
+        table_.pop_front();
+        num_of_scopes_--;
+    }
+
+    void Symbol_table::insert(std::size_t name_idx, const Name_occurence& name_occ)
+    {
+        if(!num_of_scopes_){
+            add_scope();
+        }
+        auto current = table_.front();
+        current->insert(name_idx, name_occ);
+    }
+
+    Search_result Symbol_table::search(std::size_t name_idx)
+    {
+        Search_result result;
+        if(!num_of_scopes_){
+            return Name_is_not_found{};
+        }
+        std::size_t current_scope_number = 0;
+        for(const auto& scp : table_){
+            auto current_result = scp->search(name_idx);
+            if(current_result){
+                auto info = *current_result;
+                if(num_of_scopes_ == current_scope_number){
+                    return Name_in_global_scope{info};
+                }else{
+                    return Name_in_local_scope{info};
+                }
+            }else{
+                current_scope_number++;
+            }
+        }
+        return Name_is_not_found{};
+    }
+};
