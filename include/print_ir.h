cebe2c2f5fedac3a6b0aa9ada941458f27ad3cea
GavrilovVS <vladimir.s.gavrilov@gmail.com>
2019-04-23 07:28:29 +0100

    I began to write a printing of IR.

---
+++ include/print_ir.h
@@ -0,0 +1,14 @@
+/*
+    File:    print_ir.h
+    Created: 23 April 2019 at 09:18 Moscow time
+    Author:  Гаврилов Владимир Сергеевич
+    E-mails: vladimir.s.gavrilov@gmail.com
+             gavrilov.vladimir.s@mail.ru
+             gavvs1977@yandex.ru
+*/
+
+#ifndef PRINT_IR_H
+#define PRINT_IR_H
+#   include "../include/intermediate_repres.h"
+void print_ir(const lynx_ir::Command& buf);
+#endif
---
+++ src/print_ir.cpp
@@ -0,0 +1,15 @@
+/*
+    File:    print_ir.cpp
+    Created: 23 April 2019 at 09:21 Moscow time
+    Author:  Гаврилов Владимир Сергеевич
+    E-mails: vladimir.s.gavrilov@gmail.com
+             gavrilov.vladimir.s@mail.ru
+             gavvs1977@yandex.ru
+*/
+
+#include "../include/print_ir.h"
+void print_ir(const lynx_ir::Command& buf)
+{
+    for(const auto& com : buf){
+    }
+}
